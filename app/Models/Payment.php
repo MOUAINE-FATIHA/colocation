6daef97b5019c212ef20db95af2c48ce49d0fa39
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-03-02 10:53:07 +0000

    implémentation des statistiques par catégorie et par mois

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -27,12 +27,10 @@ class Payment extends Model
     {
         return $this->belongsTo(Colocation::class);
     }
-
     public function fromUser()
     {
         return $this->belongsTo(User::class, 'from_user_id');
     }
-
     public function toUser()
     {
         return $this->belongsTo(User::class, 'to_user_id');
