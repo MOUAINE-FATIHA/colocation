c6f1fc1147a075e4f07c9ebd4b30a5f7f197fe60
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2026-03-02 10:12:55 +0000

    creation d une colocation avec attribution automatique du role owner

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
+    
 protected $fillable = ['name', 'status'];
 
 public function memberships() {
@@ -15,12 +14,11 @@ public function activeMembers() {
     return $this->hasMany(Membership::class)->whereNull('left_at');
 }
 public function owner() {
-    return $this->hasOne(Membership::class)->where('role', 'owner')->whereNull('left_at');
+    return $this->hasOne(Membership::class)->where('role','owner')->whereNull('left_at');
 }
 public function expenses() {
     return $this->hasMany(Expense::class);
 }
-
 public function invitations() {
     return $this->hasMany(Invitation::class);
 }
